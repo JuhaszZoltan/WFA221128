ce8df0ff0ea79376414c563ecefbfcee065d2873
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-11-29 10:54:03 +0100

    updt + del

--- FrmMain.Designer.cs
+++ FrmMain.Designer.cs
@@ -33,15 +33,29 @@
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tbNev = new System.Windows.Forms.TextBox();
-            this.rbFF = new System.Windows.Forms.RadioButton();
-            this.dtpSzul = new System.Windows.Forms.DateTimePicker();
-            this.rbNO = new System.Windows.Forms.RadioButton();
+            this.tbNevInsert = new System.Windows.Forms.TextBox();
+            this.rbFFInsert = new System.Windows.Forms.RadioButton();
+            this.dtpSzulInsert = new System.Windows.Forms.DateTimePicker();
+            this.rbNOInsert = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.btnInsert = new System.Windows.Forms.Button();
+            this.cbNagykoru = new System.Windows.Forms.CheckBox();
+            this.tbNevUpdate = new System.Windows.Forms.TextBox();
+            this.rbFFUpdate = new System.Windows.Forms.RadioButton();
+            this.rbNOUpdate = new System.Windows.Forms.RadioButton();
+            this.dtpSzulUpdate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgv
@@ -64,8 +78,9 @@
             this.dgv.RowHeadersVisible = false;
             this.dgv.RowTemplate.Height = 25;
             this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgv.Size = new System.Drawing.Size(855, 299);
+            this.dgv.Size = new System.Drawing.Size(846, 299);
             this.dgv.TabIndex = 0;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Dgv_CellClick);
             // 
             // Column1
             // 
@@ -91,47 +106,47 @@
             this.Column4.HeaderText = "született";
             this.Column4.Name = "Column4";
             // 
-            // textBox1
+            // tbNevInsert
             // 
-            this.tbNev.Location = new System.Drawing.Point(635, 354);
-            this.tbNev.Name = "textBox1";
-            this.tbNev.Size = new System.Drawing.Size(215, 36);
-            this.tbNev.TabIndex = 1;
+            this.tbNevInsert.Location = new System.Drawing.Point(131, 32);
+            this.tbNevInsert.Name = "tbNevInsert";
+            this.tbNevInsert.Size = new System.Drawing.Size(215, 36);
+            this.tbNevInsert.TabIndex = 1;
             // 
-            // rbFF
+            // rbFFInsert
             // 
-            this.rbFF.AutoSize = true;
-            this.rbFF.Checked = true;
-            this.rbFF.Location = new System.Drawing.Point(635, 396);
-            this.rbFF.Name = "rbFF";
-            this.rbFF.Size = new System.Drawing.Size(70, 34);
-            this.rbFF.TabIndex = 2;
-            this.rbFF.TabStop = true;
-            this.rbFF.Text = "férfi";
-            this.rbFF.UseVisualStyleBackColor = true;
+            this.rbFFInsert.AutoSize = true;
+            this.rbFFInsert.Checked = true;
+            this.rbFFInsert.Location = new System.Drawing.Point(131, 74);
+            this.rbFFInsert.Name = "rbFFInsert";
+            this.rbFFInsert.Size = new System.Drawing.Size(70, 34);
+            this.rbFFInsert.TabIndex = 2;
+            this.rbFFInsert.TabStop = true;
+            this.rbFFInsert.Text = "férfi";
+            this.rbFFInsert.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker1
+            // dtpSzulInsert
             // 
-            this.dtpSzul.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpSzul.Location = new System.Drawing.Point(635, 436);
-            this.dtpSzul.Name = "dateTimePicker1";
-            this.dtpSzul.Size = new System.Drawing.Size(215, 36);
-            this.dtpSzul.TabIndex = 3;
+            this.dtpSzulInsert.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpSzulInsert.Location = new System.Drawing.Point(131, 114);
+            this.dtpSzulInsert.Name = "dtpSzulInsert";
+            this.dtpSzulInsert.Size = new System.Drawing.Size(215, 36);
+            this.dtpSzulInsert.TabIndex = 3;
             // 
-            // rbNO
+            // rbNOInsert
             // 
-            this.rbNO.AutoSize = true;
-            this.rbNO.Location = new System.Drawing.Point(790, 396);
-            this.rbNO.Name = "rbNO";
-            this.rbNO.Size = new System.Drawing.Size(56, 34);
-            this.rbNO.TabIndex = 2;
-            this.rbNO.Text = "nő";
-            this.rbNO.UseVisualStyleBackColor = true;
+            this.rbNOInsert.AutoSize = true;
+            this.rbNOInsert.Location = new System.Drawing.Point(290, 74);
+            this.rbNOInsert.Name = "rbNOInsert";
+            this.rbNOInsert.Size = new System.Drawing.Size(56, 34);
+            this.rbNOInsert.TabIndex = 2;
+            this.rbNOInsert.Text = "nő";
+            this.rbNOInsert.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(523, 357);
+            this.label1.Location = new System.Drawing.Point(50, 35);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 30);
             this.label1.TabIndex = 4;
@@ -140,7 +155,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(515, 398);
+            this.label2.Location = new System.Drawing.Point(42, 76);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 30);
             this.label2.TabIndex = 4;
@@ -149,42 +164,172 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(479, 441);
+            this.label3.Location = new System.Drawing.Point(6, 119);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(101, 30);
             this.label3.TabIndex = 4;
             this.label3.Text = "Született:";
             // 
-            // button1
+            // btnInsert
             // 
-            this.btnInsert.Location = new System.Drawing.Point(479, 502);
-            this.btnInsert.Name = "button1";
-            this.btnInsert.Size = new System.Drawing.Size(371, 53);
+            this.btnInsert.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnInsert.Location = new System.Drawing.Point(6, 180);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(340, 53);
             this.btnInsert.TabIndex = 5;
             this.btnInsert.Text = "ÚJ SZEMÉLY HOZZÁADÁSA";
-            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.UseVisualStyleBackColor = false;
             this.btnInsert.Click += new System.EventHandler(this.BtnInsert_Click);
+            // 
+            // cbNagykoru
+            // 
+            this.cbNagykoru.AutoSize = true;
+            this.cbNagykoru.Location = new System.Drawing.Point(21, 353);
+            this.cbNagykoru.Name = "cbNagykoru";
+            this.cbNagykoru.Size = new System.Drawing.Size(123, 34);
+            this.cbNagykoru.TabIndex = 6;
+            this.cbNagykoru.Text = "nagykorú";
+            this.cbNagykoru.UseVisualStyleBackColor = true;
+            this.cbNagykoru.CheckedChanged += new System.EventHandler(this.CbNagykoru_CheckedChanged);
+            // 
+            // tbNevUpdate
+            // 
+            this.tbNevUpdate.Location = new System.Drawing.Point(131, 33);
+            this.tbNevUpdate.Name = "tbNevUpdate";
+            this.tbNevUpdate.Size = new System.Drawing.Size(215, 36);
+            this.tbNevUpdate.TabIndex = 1;
+            // 
+            // rbFFUpdate
+            // 
+            this.rbFFUpdate.AutoSize = true;
+            this.rbFFUpdate.Checked = true;
+            this.rbFFUpdate.Location = new System.Drawing.Point(131, 75);
+            this.rbFFUpdate.Name = "rbFFUpdate";
+            this.rbFFUpdate.Size = new System.Drawing.Size(70, 34);
+            this.rbFFUpdate.TabIndex = 2;
+            this.rbFFUpdate.TabStop = true;
+            this.rbFFUpdate.Text = "férfi";
+            this.rbFFUpdate.UseVisualStyleBackColor = true;
+            // 
+            // rbNOUpdate
+            // 
+            this.rbNOUpdate.AutoSize = true;
+            this.rbNOUpdate.Location = new System.Drawing.Point(290, 71);
+            this.rbNOUpdate.Name = "rbNOUpdate";
+            this.rbNOUpdate.Size = new System.Drawing.Size(56, 34);
+            this.rbNOUpdate.TabIndex = 2;
+            this.rbNOUpdate.Text = "nő";
+            this.rbNOUpdate.UseVisualStyleBackColor = true;
+            // 
+            // dtpSzulUpdate
+            // 
+            this.dtpSzulUpdate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpSzulUpdate.Location = new System.Drawing.Point(131, 115);
+            this.dtpSzulUpdate.Name = "dtpSzulUpdate";
+            this.dtpSzulUpdate.Size = new System.Drawing.Size(215, 36);
+            this.dtpSzulUpdate.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Név:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 30);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nem:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 30);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Született:";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnUpdate.Location = new System.Drawing.Point(6, 180);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(344, 53);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "KIJELÖLT MÓDOSÍTÁSA";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnInsert);
+            this.groupBox1.Controls.Add(this.tbNevInsert);
+            this.groupBox1.Controls.Add(this.rbFFInsert);
+            this.groupBox1.Controls.Add(this.dtpSzulInsert);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.rbNOInsert);
+            this.groupBox1.Location = new System.Drawing.Point(512, 332);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 239);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.rbNOUpdate);
+            this.groupBox2.Controls.Add(this.tbNevUpdate);
+            this.groupBox2.Controls.Add(this.rbFFUpdate);
+            this.groupBox2.Controls.Add(this.dtpSzulUpdate);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.btnUpdate);
+            this.groupBox2.Location = new System.Drawing.Point(150, 332);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(356, 239);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Tomato;
+            this.btnDelete.Location = new System.Drawing.Point(150, 587);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(717, 53);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "KIJELÖLT SZEMÉLY TÖRLÉSE";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(890, 583);
-            this.Controls.Add(this.btnInsert);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dtpSzul);
-            this.Controls.Add(this.rbNO);
-            this.Controls.Add(this.rbFF);
-            this.Controls.Add(this.tbNev);
+            this.ClientSize = new System.Drawing.Size(890, 667);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cbNagykoru);
             this.Controls.Add(this.dgv);
             this.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "FrmMain";
-            this.Text = "Form1";
+            this.Text = "példa CRUDra";
             this.Load += new System.EventHandler(this.FrmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,13 +342,25 @@
         private DataGridViewTextBoxColumn Column2;
         private DataGridViewTextBoxColumn Column3;
         private DataGridViewTextBoxColumn Column4;
-        private TextBox tbNev;
-        private RadioButton rbFF;
-        private DateTimePicker dtpSzul;
-        private RadioButton rbNO;
+        private TextBox tbNevInsert;
+        private RadioButton rbFFInsert;
+        private DateTimePicker dtpSzulInsert;
+        private RadioButton rbNOInsert;
         private Label label1;
         private Label label2;
         private Label label3;
         private Button btnInsert;
+        private CheckBox cbNagykoru;
+        private TextBox tbNevUpdate;
+        private RadioButton rbFFUpdate;
+        private RadioButton rbNOUpdate;
+        private DateTimePicker dtpSzulUpdate;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button btnUpdate;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button btnDelete;
     }
 }
--- FrmMain.cs
+++ FrmMain.cs
@@ -5,6 +5,8 @@ namespace WFA221128
 {
     public partial class FrmMain : Form
     {
+        private int selectedId = -1;
+
         public FrmMain()
         {
             InitializeComponent();
@@ -12,11 +14,15 @@ namespace WFA221128
 
         private void DgvReLoad()
         {
+            ClearFields();
+            string where = " WHERE DATEADD(year, -18, GETDATE()) > szul";
+
             dgv.Rows.Clear();
             using SqlConnection conn = new(Resources.ConnectionString);
             conn.Open();
             SqlDataReader rdr = new SqlCommand(
-                "SELECT * FROM emberek;",
+                "SELECT * FROM emberek" +
+                $"{(cbNagykoru.Checked ? where : null)};",
                 conn).ExecuteReader();
 
             while (rdr.Read())
@@ -27,32 +33,25 @@ namespace WFA221128
                     rdr.GetBoolean(2) ? "férfi" : "nõ",
                     rdr.GetDateTime(3).ToString("yyyy. MMM dd."));
             }
+            dgv.ClearSelection();
         }
 
         private void FrmMain_Load(object sender, EventArgs e) => DgvReLoad();
 
         private void BtnInsert_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(tbNev.Text))
+            if (KiVanToltve(tbNevInsert.Text))
             {
-                _ = MessageBox.Show(
-                    caption: "HIBA",
-                    text: "A 'név' mezõ kitöltése kötelezõ!",
-                    buttons: MessageBoxButtons.OK,
-                    icon: MessageBoxIcon.Error);
-            }
-            else
-            {
-                string nev = tbNev.Text;
-                string nem = rbFF.Checked ? "1" : "0";
-                string szul = dtpSzul.Value.ToString("yyyy-MM-dd");
+                string nev = tbNevInsert.Text;
+                string nem = rbFFInsert.Checked ? "1" : "0";
+                string szul = dtpSzulInsert.Value.ToString("yyyy-MM-dd");
 
                 using SqlConnection conn = new(Resources.ConnectionString);
                 conn.Open();
 
                 SqlDataAdapter sda = new()
                 {
-                    InsertCommand = new SqlCommand(
+                    InsertCommand = new(
                         cmdText: "INSERT INTO emberek VALUES " +
                         $"('{nev}', {nem}, '{szul}');",
                         connection: conn),
@@ -63,9 +62,101 @@ namespace WFA221128
                 //_ = new SqlCommand("<SQL utasítás>", conn).ExecuteNonQuery();
                 DgvReLoad();
             }
+        }
 
+        private void ClearFields()
+        {
+            tbNevInsert.Text = null;
+            tbNevUpdate.Text = null;
+            rbFFInsert.Checked = true;
+            rbFFUpdate.Checked = true;
+            dtpSzulInsert.Value = DateTime.Now;
+            dtpSzulUpdate.Value = DateTime.Now;
+            selectedId = -1;
+            //cbNagykoru.Checked = false;
+        }
 
+        private void CbNagykoru_CheckedChanged(object sender, EventArgs e)
+        {
+            DgvReLoad();
+        }
 
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            if (KiVanToltve(tbNevUpdate.Text))
+            {
+                using SqlConnection conn = new(Resources.ConnectionString);
+                conn.Open();
+
+                SqlDataAdapter sda = new()
+                {
+                    UpdateCommand = new(
+                        cmdText: "UPDATE emberek SET " +
+                        $"nev = '{tbNevUpdate.Text}', " +
+                        $"nem = {(rbFFUpdate.Checked ? 1 : 0)}, " +
+                        $"szul = '{dtpSzulUpdate.Value.ToString("yyyy-MM-dd")}' " +
+                        $"WHERE id = {selectedId};",
+                        connection: conn),
+                };
+                sda.UpdateCommand.ExecuteNonQuery();
+                DgvReLoad();
+            }
+        }
+
+        private bool KiVanToltve(string mezo)
+        {
+            if (string.IsNullOrWhiteSpace(mezo))
+            {
+                _ = MessageBox.Show(
+                   caption: "HIBA",
+                   text: "A 'név' mezõ kitöltése kötelezõ!",
+                   buttons: MessageBoxButtons.OK,
+                   icon: MessageBoxIcon.Error);
+                return false;
+            }
+            else return true;
+        }
+
+        private void Dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedId = (int)dgv[0, e.RowIndex].Value;
+            tbNevUpdate.Text = $"{dgv[1, e.RowIndex].Value}";
+            if ($"{dgv[2, e.RowIndex].Value}" == "férfi")
+            {
+                rbFFUpdate.Checked = true;
+                rbNOUpdate.Checked = false;
+            }
+            else
+            {
+                rbNOUpdate.Checked = true;
+                rbFFUpdate.Checked = false;
+            }
+            dtpSzulUpdate.Value = DateTime.Parse($"{dgv[3, e.RowIndex].Value}");
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            var res = MessageBox.Show(
+                caption: "MEGERÕSÍTÉS",
+                text: "Biztos, hogy törölni kívánod a kijelölt rekordot?",
+                buttons: MessageBoxButtons.YesNo,
+                icon: MessageBoxIcon.Warning);
+
+            if (res == DialogResult.Yes)
+            {
+                using SqlConnection conn = new(Resources.ConnectionString);
+                conn.Open();
+
+                SqlDataAdapter sda = new()
+                {
+                    DeleteCommand = new(
+                        $"DELETE FROM emberek WHERE " +
+                        $"id = {selectedId};",
+                        conn),
+                };
+                sda.DeleteCommand.ExecuteNonQuery();
+                DgvReLoad();
+            }
         }
     }
 }
